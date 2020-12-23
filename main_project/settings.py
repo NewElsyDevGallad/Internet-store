42bf2adb1794a6a3493a54dc4b4e9a207bf800f6
Gallad <Gallad@gal.con>
2020-12-23 13:14:48 +0700

    DEBUG -> True...again(

--- main_project/settings.py
+++ main_project/settings.py
@@ -39,7 +39,7 @@ STATICFILES_FINDERS = (
 SECRET_KEY = 'e*vm&m8m0%h*_k=u&2$t3#_)#m)ci1k1y)*z^*9-kc7y=cm+ts'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
