f6f61d8a71e196d350dbc45bc98aa3e5977bce2f
Gallad <Gallad@gal.con>
2020-12-23 13:21:46 +0700

    django.conf.urls.static, not django.conf.urls

--- main_project/urls.py
+++ main_project/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from shop import views
