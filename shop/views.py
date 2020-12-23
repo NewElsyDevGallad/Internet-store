de3bced9108a4838b79e69a4a83b0b3c1e601584
Gallad <Gallad@gal.con>
2020-12-23 15:24:33 +0700

    fix errors 2

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from shop.models import Category, Product
 from cart.forms import CartAddProductForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
