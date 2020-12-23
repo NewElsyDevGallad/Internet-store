1577d1ef520a3b9413f1a52b79c582a22ff2d72f
Gallad <Gallad@gal.con>
2020-12-23 13:42:57 +0700

    1

--- main_project/urls.py
+++ main_project/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
 
 # В конце файла:
 if settings.DEBUG:
-    if settings.MEDIA_ROOT:
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Эта строка опциональна и будет добавлять url'ы только при DEBUG = True
 urlpatterns += staticfiles_urlpatterns()
