cf0c36718c67737e5ee7fa3e1f172a4cd41f6718
Gallad <Gallad@gal.con>
2020-12-25 11:18:59 +0700

    Work with pagination and search

--- advice_post/urls.py
+++ advice_post/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post_slug>[-\w]+)/$',
+    url(r'^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/(?P<post_slug>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
     url(r'^searched$', views.post_search, name='post_search'),
